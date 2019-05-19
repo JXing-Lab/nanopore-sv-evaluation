37675d7907fa86633a7fc1f03031b7275b952fc7
Xing Lab of Genomics <xing@biology.rutgers.edu>
2019-05-18 20:14:36 -0400

    Update integration.sh

--- integration.sh
+++ integration.sh
@@ -1,21 +1,11 @@
-cat graphmap-nanosv/nanosv.chr.del.sort.merge.bed graphmap-sniffles/sniffles.chr.del.sort.merge.bed ngmlr-nanosv/nanosv.chr.del.sort.merge.bed ngmlr-sniffles/sniffles.chr.del.sort.merge.bed minimap2-nanosv/nanosv.chr.del.sort.merge.bed minimap2-sniffles/sniffles.chr.del.sort.merge.bed last-picky/picky.allsv.chr.del.sort.merge.bed > cat.del.bed
+awk '{print $0"\t"FILENAME}' graphmap-nanosv/nanosv.chr.del.sort.merge.bed graphmap-sniffles/sniffles.chr.del.sort.merge.bed ngmlr-nanosv/nanosv.chr.del.sort.merge.bed ngmlr-sniffles/sniffles.chr.del.sort.merge.bed minimap2-nanosv/nanosv.chr.del.sort.merge.bed minimap2-sniffles/sniffles.chr.del.sort.merge.bed last-picky/picky.allsv.chr.del.sort.merge.bed > cat.del.bed
 
-cat graphmap-nanosv/nanosv.chr.ins.bp.sort.merge.bed graphmap-sniffles/sniffles.chr.ins.bp.sort.merge.bed ngmlr-nanosv/nanosv.chr.ins.bp.sort.merge.bed ngmlr-sniffles/sniffles.chr.ins.bp.sort.merge.bed minimap2-nanosv/nanosv.chr.ins.bp.sort.merge.bed minimap2-sniffles/sniffles.chr.ins.bp.sort.merge.bed last-picky/picky.allsv.chr.ins.bp.sort.merge.bed > cat.ins.bed
+awk '{print $0"\t"FILENAME}' graphmap-nanosv/nanosv.chr.ins.bp.sort.merge.bed graphmap-sniffles/sniffles.chr.ins.bp.sort.merge.bed ngmlr-nanosv/nanosv.chr.ins.bp.sort.merge.bed ngmlr-sniffles/sniffles.chr.ins.bp.sort.merge.bed minimap2-nanosv/nanosv.chr.ins.bp.sort.merge.bed minimap2-sniffles/sniffles.chr.ins.bp.sort.merge.bed last-picky/picky.allsv.chr.ins.bp.sort.merge.bed > cat.ins.bed
 
-bedtools sort -i cat.del.bed | bedtools merge -c 1 -o count > merged.del.bed
+bedtools sort -i cat.del.bed | bedtools merge -c 5 -o count_distinct > integrated.del.bed
 
-bedtools sort -i cat.ins.bed | bedtools merge -c 1 -d 100 -o count > merged.ins.bed
+bedtools sort -i cat.ins.bed | bedtools merge -d 100 -c 5 -o count_distinct > integrated.ins.bed
 
-awk '$4>=2{print}' merged.del.bed > consensus2.del.bed
-awk '$4>=3{print}' merged.del.bed > consensus3.del.bed
-awk '$4>=4{print}' merged.del.bed > consensus4.del.bed
-awk '$4>=5{print}' merged.del.bed > consensus5.del.bed
-awk '$4>=6{print}' merged.del.bed > consensus6.del.bed
-awk '$4>=7{print}' merged.del.bed > consensus7.del.bed
+for i in `seq 2 7`; do awk '$4>=i{print}' i=$i integrated.ins.bed > let$i.ins.bed; done
 
-awk '$4>=2{print}' merged.ins.bed > consensus2.ins.bed
-awk '$4>=3{print}' merged.ins.bed > consensus3.ins.bed
-awk '$4>=4{print}' merged.ins.bed > consensus4.ins.bed
-awk '$4>=5{print}' merged.ins.bed > consensus5.ins.bed
-awk '$4>=6{print}' merged.ins.bed > consensus6.ins.bed
-awk '$4>=7{print}' merged.ins.bed > consensus7.ins.bed
+for i in `seq 2 7`; do awk '$4>=i{print}' i=$i integrated.del.bed > let$i.del.bed; done
