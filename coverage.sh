3b5879ba29b742d01da0d6dd3c07de48dfdd7e2f
Xing Lab of Genomics <xing@biology.rutgers.edu>
2019-05-21 17:28:32 -0400

    Update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,8 @@
+# subsetting fasta reads
 seqtk sample -s seed=11 simulated_reads.fasta 0.2 > simulated_reads_10x.fasta
 
-/usr/bin/time -v minimap2 -ax map-ont -t 16 --MD /nfs/XingLab_ISI/Nanopore/Evaluation/Reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna /nfs/XingLab_ISI/Nanopore/Coverage/rel3-nanopore-wgs_10x.fastq | samtools sort -o rel3-nanopore-wgs_10x.bam
+# map using minimap2
+/usr/bin/time -v minimap2 -ax map-ont -t 16 --MD ref.fna simulated_reads_10x.fastq | samtools sort -o mapped_10x.bam
 
-sniffles -m rel3-nanopore-wgs_10x.bam -v rel3-nanopore-wgs_10x.vcf
+# SV call using sniffles
+sniffles -m mapped_10x.bam -v mapped_10x.vcf
