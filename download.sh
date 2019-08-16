80942313cbc7f540a126faf27937064945c56bed
Xing Lab of Genomics <xing@biology.rutgers.edu>
2019-08-16 15:12:24 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,9 +8,15 @@ bash download.sh
 # concatenate fastq
 cat *.fastq > cat.fastq
 
+# download fastq reads for CHM13
+wget https://s3.amazonaws.com/nanopore-human-wgs/chm13/nanopore/rel2/rel2.fastq.gz
+
 # download SV calls for NA12878
 wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/NA12878/NA12878_PacBio_MtSinai/NA12878.sorted.vcf.gz
 wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/NA12878/NA12878_PacBio_MtSinai/NA12878.sorted.vcf.gz.tbi
+
+# download SV calls for CHM13
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/dbVar/data/Homo_sapiens/by_study/vcf/nstd137.GRCh38.variant_call.vcf.gz
 
 # download SV calls for CHM1
 wget http://eichlerlab.gs.washington.edu/publications/chm1-structural-variation/data/GRCh37/insertions.bed
